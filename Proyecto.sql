10053dc4c59e574ae96375dac284ecf1dbe6eb46
CesarH-IbarraN <minichamanred@gmail.com>
2018-11-03 13:27:39 -0600

    Update Proyecto.sql

--- Proyecto.sql
+++ Proyecto.sql
@@ -2,7 +2,7 @@ insert into Salones values(1,101)
 insert into Periodo values(1,'manana', '8:00','12:00')
 insert into Periodo values(2,'tarde', '12:00','4:00')
 insert into Periodo values(3,'noche', '4:00','8:00')
-insert into Estados values(1,'Nuevo Le�n')
+insert into Estados values(1,'Nuevo León')
 insert into Municipio values(1,'San Nicolas',1)
 insert into Municipio values(2,'Monterrey',1)
 insert into Municipio values(3,'Santiago',1)
@@ -17,7 +17,7 @@ insert into Carrera values(1,1,8,'LSTI')
 insert into Carrera values(2,1,9,'Actuaria')
 insert into Carrera values(3,1,7,'LCC')
 insert into Alumnos values(1,1,'Carlos','Ibarra','Ojeda','4/8/16',1,1)
-insert into Alumnos values(2,2,'C�sar','Ibarra','Nungaray','4/8/16',1,1)
+insert into Alumnos values(2,2,'César','Ibarra','Nungaray','4/8/16',1,1)
 insert into Alumnos values(3,3,'Guillermo','Tormo','Belmonte','4/8/16',1,1)
 insert into Alumnos values(4,4,'Alvaro','Canovas','Salmeron','4/8/16',1,1)
 insert into Alumnos values(5,5,'Antonio','Torrejon','Torras','4/8/16',1,1)
@@ -51,7 +51,7 @@ insert into Alumnos values(32,32,'Manuel','Caballo','Del Rosario','4/8/16',1,1)
 insert into Alumnos values(33,33,'Juan','Escriva','Tacilla','4/8/16',1,1)
 insert into Alumnos values(34,34,'Alvaro','Segovia','Lamas','4/8/16',1,1)
 insert into Alumnos values(35,35,'Domingo','Moll','Salvador','4/8/16',1,1)
-insert into Alumnos values(36,36,'Manuel','Tartajada','Due�as','4/8/16',1,1)
+insert into Alumnos values(36,36,'Manuel','Tartajada','Dueñas','4/8/16',1,1)
 insert into Alumnos values(37,37,'Joaquin','Romeo','Lucas','4/8/16',1,1)
 insert into Alumnos values(38,38,'Tomas','Jaime','Rivas','4/8/16',1,1)
 insert into Alumnos values(39,39,'Jose','Villalva','Busquets','4/8/16',1,1)
@@ -73,3 +73,254 @@ insert into Alumnos values(54,54,'Jose','Cano','Blasco','4/8/16',1,1)
 insert into Alumnos values(55,55,'Mohamed','De la Hoz','Potero','4/8/16',1,1)
 insert into Alumnos values(56,56,'David','Sabater','Soled','4/8/16',1,1)
 insert into Alumnos values(57,57,'Carlos','Carratero','Sevilla','4/8/16',1,1)
+
+select [Nombre(s)] as Nombre
+from Alumnos
+group by [Nombre(s)]
+
+select [Apellido Paterno], Matrícula
+from Alumnos
+
+select Nombre as Locación
+from Estados
+
+select Matrícula, [Nombre(s)]
+from Alumnos
+where Matrícula>30 and Matrícula<50
+
+select [Nombre(s)] as Reprobados
+from Alumnos
+where Matrícula<20
+
+select Direcciones, [Nombre(s)], Matrícula, Direcciones.Colonia
+from Alumnos
+INNER JOIN Direcciones ON
+Direcciones=[Id Direcciones]
+
+select Direcciones, [Nombre(s)], Matrícula, Direcciones.Colonia
+from Alumnos
+RIGHT JOIN Direcciones ON
+Direcciones=[Id Direcciones]
+
+select Direcciones, [Nombre(s)], Matrícula, Direcciones.Colonia
+from Alumnos
+FULL JOIN Direcciones ON
+Direcciones=[Id Direcciones]
+
+select Direcciones, [Nombre(s)], Matrícula, Direcciones.Colonia
+from Alumnos
+LEFT JOIN Direcciones ON
+Direcciones=[Id Direcciones]
+
+select [Nombre(s)] as Reprobados
+from Alumnos
+where Matrícula<20
+order by [Nombre(s)]
+
+select Nombre
+from Carrera
+where [Id Carrera]=2
+
+select Calle
+from Direcciones
+where Colonia = 1
+
+select Número
+from Salones
+where Número>1
+
+select Nombre
+from Periodo
+where Inicio='8:00'
+
+select min(Matrícula)
+from Alumnos
+where [Nombre(s)]='Carlos'
+group by Matrícula
+having Matrícula=1
+
+select max(Matrícula)
+from Alumnos
+where [Nombre(s)]='Carlos'
+group by Matrícula
+
+select avg(Matrícula)
+from Alumnos
+group by [Nombre(s)]
+
+select count([Nombre(s)])
+from Alumnos
+where [Nombre(s)]=('Francisco')
+              
+       USE empleado
+
+Select Salario, COUNT(Salario) as 'salarios'
+from empleado_tbl
+group by Salario
+having Salario>38000
+
+select PrimerNombre
+from empleado_tbl
+where Numero = 6276648992
+
+select PrimerNombre, Salario
+From empleado_tbl
+where Numero>8000000000
+
+with Salarios as (select Salario, count(Salario) as SS
+from empleado_tbl
+group by Salario)
+Select* from Salarios
+where Salarios.SS>1
+
+
+
+
+create view view1 
+as select Salario, COUNT(Salario) as 'salarios'
+from empleado_tbl
+group by Salario
+having Salario>38000
+
+create view view2
+as select PrimerNombre
+from empleado_tbl
+where Numero = 6276648992
+
+create view view3
+as select PrimerNombre, Salario
+From empleado_tbl
+where Numero>8000000000
+
+create view view4
+as with Salarios as (select Salario, count(Salario) as SS
+from empleado_tbl
+group by Salario)
+Select* from Salarios
+where Salarios.SS>1
+
+create view view5
+as select PrimerNombre, Apellido
+from empleado_tbl
+
+Declare @query nvarchar(50)
+declare @nomcol nvarchar(50) = 'Nombre'
+set @query = 'select PrimerNombre as '+ @nomcol + ' from empleado_tbl'
+print @query
+              
+              create procedure [dbo].[Escue](@valor1 int)
+as
+begin
+	declare @reporte nvarchar(50)
+	set @reporte = 'Hola'
+	print @reporte
+	select * from dbo.Alumnos
+	where Matrícula>@valor1
+end
+
+exec Escue 30
+go
+
+create procedure [dbo].[dos](@valor1 int)
+as
+begin
+	declare @reporte nvarchar(50)
+	set @reporte = 'Hola'
+	print @reporte
+	select * from dbo.Alumnos
+	where Matrícula<@valor1
+end
+go
+
+exec dos 20
+go
+
+create procedure [dbo].[tres](@valor1 int)
+as
+begin
+	declare @reporte nvarchar(50)
+	set @reporte = 'Hola'
+	print @reporte
+	select * from dbo.Salones
+	where Número>@valor1
+end
+go
+exec tres 2
+go
+
+create procedure [dbo].[cuatro](@valor1 time)
+as
+begin
+	declare @reporte nvarchar(50)
+	set @reporte = 'Hola'
+	print @reporte
+	select * from dbo.Periodo
+	where Final=@valor1
+end
+go
+exec cuatro '12:00'
+go
+
+create procedure [dbo].[cinco](@valor1 varchar(50))
+as
+begin
+	declare @reporte nvarchar(50)
+	set @reporte = 'Hola'
+	print @reporte
+	select * from dbo.Alumnos
+	where Direcciones=@valor1
+end
+go
+
+exec cinco 2
+go
+
+create function dbo.fnmul (@v1 float,@v2 float)
+returns float
+as
+begin
+	return @v1*@v2
+end
+go
+
+select dbo.fnmul(5,4)
+                                               
+                                             Set ANSI_NULLS ON
+GO
+Set QUOTED_IDENTIFIER ON
+GO
+
+Use Escuela
+go
+
+ALTER trigger [dbo].[tr_insert]
+on [dbo].[Alumnos]
+instead of insert
+as
+begin
+	select*
+	from inserted
+END
+go
+
+CREATE trigger [dbo].[tr_update]
+on [dbo].[Carrera]
+after update
+as
+begin
+	select*
+	from inserted
+	select*
+	from deleted
+END
+go
+
+CREATE trigger [dbo].[tr_delete]
+on [dbo].[Periodo]
+instead of delete
+as
+begin
+	select*
+	from deleted
+END
+go  
